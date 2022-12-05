ecdb6fcb54590aedab37c101a172e347594641c9
Israel Gonzalez <igonzale@trinity.edu>
2022-12-05 13:25:21 -0600

    Main game looks nicer, rn only diff between 'story' and 'endless' is that the score only shows in endless, this will change

--- Assets/Scripts/IsraelMinigameScripts/PHGame2Script.cs
+++ Assets/Scripts/IsraelMinigameScripts/PHGame2Script.cs
@@ -11,7 +11,7 @@ public class PHGame2Script : MonoBehaviour
     int score;
     int cat;
     public Text gt;
-    public InputField inp;
+    public Text inp;
     int variation;
     [SerializeField] GameObject word1;
     [SerializeField] GameObject word2;
@@ -42,30 +42,90 @@ public class PHGame2Script : MonoBehaviour
     }
     void Update() 
     {
-        if(inp.text == "nincompoop" && variation == 1) 
+        if(Input.GetKeyDown(KeyCode.A))
         {
-            score += 5;
-            cat = 2;
-            PlayerPrefs.SetInt("score", score);
-            PlayerPrefs.SetInt("cat", cat);
-            SceneManager.LoadScene("MainGame");
-        }
-        if(inp.text == "blackjacks" && variation == 2) 
+            inp.text += 'a';
+        } else if (Input.GetKeyDown(KeyCode.B))
         {
-            score += 5;
-            cat = 2;
-            PlayerPrefs.SetInt("score", score);
-            PlayerPrefs.SetInt("cat", cat);
-            SceneManager.LoadScene("MainGame");
-        }
-        if(inp.text == "applejacks" && variation == 3) 
+            inp.text += 'b';
+        } else if (Input.GetKeyDown(KeyCode.C))
         {
-            score += 5;
-            cat = 2;
-            PlayerPrefs.SetInt("score", score);
-            PlayerPrefs.SetInt("cat", cat);
-            SceneManager.LoadScene("MainGame");
+            inp.text += 'c';
+        } else if (Input.GetKeyDown(KeyCode.D))
+        {
+            inp.text += 'd';
+        } else if (Input.GetKeyDown(KeyCode.E))
+        {
+            inp.text += 'e';
+        } else if (Input.GetKeyDown(KeyCode.F))
+        {
+            inp.text += 'f';
+        } else if (Input.GetKeyDown(KeyCode.G))
+        {
+            inp.text += 'g';
+        } else if (Input.GetKeyDown(KeyCode.H))
+        {
+            inp.text += 'h';
+        } else if (Input.GetKeyDown(KeyCode.I))
+        {
+            inp.text += 'i';
+        } else if (Input.GetKeyDown(KeyCode.J))
+        {
+            inp.text += 'j';
+        } else if (Input.GetKeyDown(KeyCode.K))
+        {
+            inp.text += 'k';
+        } else if (Input.GetKeyDown(KeyCode.L))
+        {
+            inp.text += 'l';
+        } else if (Input.GetKeyDown(KeyCode.M))
+        {
+            inp.text += 'm';
+        } else if (Input.GetKeyDown(KeyCode.N))
+        {
+            inp.text += 'm';
+        } else if (Input.GetKeyDown(KeyCode.O))
+        {
+            inp.text += 'o';
+        } else if (Input.GetKeyDown(KeyCode.P))
+        {
+            inp.text += 'p';
+        } else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            inp.text += 'q';
+        } else if (Input.GetKeyDown(KeyCode.R))
+        {
+            inp.text += 'r';
+        } else if (Input.GetKeyDown(KeyCode.S))
+        {
+            inp.text += 's';
+        } else if (Input.GetKeyDown(KeyCode.T))
+        {
+            inp.text += 't';
+        } else if (Input.GetKeyDown(KeyCode.U))
+        {
+            inp.text += 'u';
+        } else if (Input.GetKeyDown(KeyCode.V))
+        {
+            inp.text += 'v';
+        } else if (Input.GetKeyDown(KeyCode.W))
+        {
+            inp.text += 'w';
+        } else if (Input.GetKeyDown(KeyCode.X))
+        {
+            inp.text += 'x';
+        } else if (Input.GetKeyDown(KeyCode.Y))
+        {
+            inp.text += 'y';
+        } else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            inp.text += 'z';
+        } else if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            inp.text = inp.text.Substring(0, inp.text.Length-1);
         }
+
+        
     }
     void FixedUpdate()
     {
@@ -75,11 +135,35 @@ public class PHGame2Script : MonoBehaviour
 
             if (timer <= 0)
             {
-                lives -= 1;
-                cat = 1;
-                PlayerPrefs.SetInt("lives", lives);
-                PlayerPrefs.SetInt("cat", cat);
-                SceneManager.LoadScene("MainGame");
+                if(inp.text == "nincompoop" && variation == 1) 
+                {
+                    score += 5;
+                    cat = 2;
+                    PlayerPrefs.SetInt("score", score);
+                    PlayerPrefs.SetInt("cat", cat);
+                    SceneManager.LoadScene("MainGame");
+                } else if(inp.text == "blackjacks" && variation == 2) 
+                {
+                    score += 5;
+                    cat = 2;
+                    PlayerPrefs.SetInt("score", score);
+                    PlayerPrefs.SetInt("cat", cat);
+                    SceneManager.LoadScene("MainGame");
+                } else if(inp.text == "applejacks" && variation == 3) 
+                {
+                    score += 5;
+                    cat = 2;
+                    PlayerPrefs.SetInt("score", score);
+                    PlayerPrefs.SetInt("cat", cat);
+                    SceneManager.LoadScene("MainGame");
+                } else 
+                {
+                    lives -= 1;
+                    cat = 1;
+                    PlayerPrefs.SetInt("lives", lives);
+                    PlayerPrefs.SetInt("cat", cat);
+                    SceneManager.LoadScene("MainGame");
+                }
             }
         }
     }
